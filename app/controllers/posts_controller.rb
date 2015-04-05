06c43ddacde852753a3851aab2836b59f7fcc5b8
xunrongl <lixunrong@gmail.com>
2015-04-05 16:22:53 -0400

    ignore authen_token

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+  skip_before_filter :verify_authenticity_token
   # GET /posts
   # GET /posts.json
   def index
